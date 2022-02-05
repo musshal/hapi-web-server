665ed2f34ff3ec39d79857af063f4e144de1f0b6
Musthafa Faishal <musthafafaishal@gmail.com>
2022-02-06 03:47:04 +0700

    Update method/verb request & routing

---
+++ routes.js
@@ -0,0 +1,39 @@
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'homepage';
+    },
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => {
+      return 'About page';
+    },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return 'Halaman tidak ditemukan';
+    },
+  },
+];
+
+module.exports = routes;
